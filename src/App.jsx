276961fe502198aeab643946d29198c79b05d4ed
rubaiyat12 <rubaiyatraha12@gmail.com>
2023-10-30 11:15:52 -0700

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./pages/Navbar";
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedPage, setSelectedPage] = useState("home");
+  
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
 
   return (
-    <>
-     
-    </>
+   <div className="app bg-deep-blue">
+    <Navbar
+        
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+
+   </div>
   )
 }
 
